f0fea11d5a260853e3aff4ead69384b162db1d11
GD <glicinia95@gmail.com>
2026-05-04 15:25:01 +0100

    changes to loop vnet creation

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
 resource "azurerm_resource_group" "rg" {
-  for_each = var.storage_accounts
+  for_each = vnet_resource
   name     = each.value.rg_name
   location = "UK South"
 }
 
-resource "azurerm_storage_account" "storage" {
-  for_each                 = var.storage_accounts
-  name                     = each.value.storage_name
-  resource_group_name      = azurerm_resource_group.rg[each.key].name
-  location                 = azurerm_resource_group.rg[each.key].location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_virtual_network" "example" {
+  for_each  = var.vnet_resource
+  name                = each.value.vnet_name
+  location            = azurerm_resource_group.rg[each.key].location
+  resource_group_name = azurerm_resource_group.rg[each.key].name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = each.value.snet_name1
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = each.value.snet_name2
+    address_prefixes = ["10.0.2.0/24"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
-variable "storage_accounts" {
+variable "vnet_resource" {
   type        = map(object({
     rg_name = string
-    storage_name = string
+    vnet_name = string
+    snet_name1 = string
+    snet_name2 = string
 }))
 }
