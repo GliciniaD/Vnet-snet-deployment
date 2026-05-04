85429e3d1861a8f6985b8ce637583458916d0ae2
GD <glicinia95@gmail.com>
2026-05-04 16:21:49 +0100

    expecting string on snet

--- main.tf
+++ main.tf
@@ -10,15 +10,14 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.rg[each.key].location
   resource_group_name = azurerm_resource_group.rg[each.key].name
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
     name             = each.value.snet_name1
-    address_prefix = ["10.0.1.0/24"]
+    address_prefix = "10.0.1.0/24"
   }
 
   subnet {
     name             = each.value.snet_name2
-    address_prefix = ["10.0.2.0/24"]
+    address_prefix = "10.0.2.0/24"
   }
 }
